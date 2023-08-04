8a153b160ed7df745bed173f77eac888074b05fc
Facundo Delavalle <delavallef4@gmail.com>
2023-08-04 19:24:41 -0300

    Changed input error message

--- Frontend/src/components/HotelForm/index.schema.ts
+++ Frontend/src/components/HotelForm/index.schema.ts
@@ -2,43 +2,83 @@ import { TypeOf, z } from 'zod';
 
 const SetHotelFormSchema = z.object({
   uniformeDesde: z
-    .number()
+    .number({
+      invalid_type_error: "Uniforme Desde debe ser un numero",
+    })
     .int('El numero debe ser un entero')
     .min(0, 'Uniforme Desde debe ser mayor a 0'),
   uniformeHasta: z
-    .number()
+    .number({
+      invalid_type_error: "Uniforme Hasta debe ser un numero",
+    })
     .int('El numero debe ser un entero')
     .min(0, 'Uniforme Hasta debe ser mayor a 0'),
   cantidadDiasGenerar: z
-    .number()
+    .number({
+      invalid_type_error: "Cantidad de dias a generar debe ser un numero",
+    })
     .int('El numero debe ser un entero')
     .min(0, 'Cantidad de dias a generar debe ser mayor a 0'),
   diaDesde: z
-    .number()
+    .number({
+      invalid_type_error: "Dia Desde debe ser un numero",
+    })
     .int('El numero debe ser un entero')
     .min(0, 'Dia desde debe ser mayor a 0'),
   diaHasta: z
-    .number()
+    .number({
+      invalid_type_error: "Dia Hasta debe ser un numero",
+    })
     .int('El numero debe ser un entero')
     .min(0, 'Dia hasta debe ser mayor a 0'),
   cantidadHabitaciones: z
-    .number()
+    .number({
+      invalid_type_error: "Cantidad de habitaciones debe ser un numero",
+    })
     .int('El numero debe ser un entero')
     .min(0, 'Cantidad de habitacion debe ser mayor a 0'),
-  precioHabitacion: z.number().min(0, 'Precio de habitacion debe ser mayor a 0'),
-  costoHabitacionOcupada: z.number().min(0, 'Costo de habitacion ocupada debe ser mayor a 0'),
-  costoHabitacionDesocupada: z.number().min(0, 'Costo de habitacion desocupada debe ser mayor a 0'),
-  costoRecibirPersonaSinLugar: z.number().min(0, 'Costo de recibir persona sin lugar debe ser mayor a 0'),
-  reservaSinAsistencia0: z.number().min(0, 'Debe ser mayor a 0'),
-  reservaSinAsistencia1: z.number().min(0, 'Debe ser mayor a 0'),
-  reservaSinAsistencia2: z.number().min(0, 'Debe ser mayor a 0'),
-  reservaSinAsistencia3: z.number().min(0, 'Debe ser mayor a 0'),
-  reservaSinAsistencia4: z.number().min(0, 'Debe ser mayor a 0'),
-  reservaSinAsistencia5: z.number().min(0, 'Debe ser mayor a 0'),
-  reservaSinAsistencia6: z.number().min(0, 'Debe ser mayor a 0'),
-  reservaSinAsistencia7: z.number().min(0, 'Debe ser mayor a 0'),
-  reservaSinAsistencia8: z.number().min(0, 'Debe ser mayor a 0'),
-  reservaSinAsistencia9: z.number().min(0, 'Debe ser mayor a 0'),
+  precioHabitacion: z.number({
+      invalid_type_error: "Precio Habitacion debe ser un numero",
+    }).min(0, 'Precio de habitacion debe ser mayor a 0'),
+  costoHabitacionOcupada: z.number({
+      invalid_type_error: "Costo Habitacion Ocupada debe ser un numero",
+    }).min(0, 'Costo de habitacion ocupada debe ser mayor a 0'),
+  costoHabitacionDesocupada: z.number({
+      invalid_type_error: "Costo Habitacion Desocupada debe ser un numero",
+    }).min(0, 'Costo de habitacion desocupada debe ser mayor a 0'),
+  costoRecibirPersonaSinLugar: z.number({
+      invalid_type_error: "Costo Recibir Personas Sin Lugar debe ser un numero",
+    }).min(0, 'Costo de recibir persona sin lugar debe ser mayor a 0'),
+  reservaSinAsistencia0: z.number({
+      invalid_type_error: "El valor debe ser un numero",
+    }).min(0, 'Debe ser mayor a 0'),
+  reservaSinAsistencia1: z.number({
+      invalid_type_error: "El valor debe ser un numero",
+    }).min(0, 'Debe ser mayor a 0'),
+  reservaSinAsistencia2: z.number({
+      invalid_type_error: "El valor debe ser un numero",
+    }).min(0, 'Debe ser mayor a 0'),
+  reservaSinAsistencia3: z.number({
+      invalid_type_error: "El valor debe ser un numero",
+    }).min(0, 'Debe ser mayor a 0'),
+  reservaSinAsistencia4: z.number({
+      invalid_type_error: "El valor debe ser un numero",
+    }).min(0, 'Debe ser mayor a 0'),
+  reservaSinAsistencia5: z.number({
+      invalid_type_error: "El valor debe ser un numero",
+    }).min(0, 'Debe ser mayor a 0'),
+  reservaSinAsistencia6: z.number({
+      invalid_type_error: "El valor debe ser un numero",
+    }).min(0, 'Debe ser mayor a 0'),
+  reservaSinAsistencia7: z.number({
+      invalid_type_error: "El valor debe ser un numero",
+    }).min(0, 'Debe ser mayor a 0'),
+  reservaSinAsistencia8: z.number({
+      invalid_type_error: "El valor debe ser un numero",
+    }).min(0, 'Debe ser mayor a 0'),
+  reservaSinAsistencia9: z.number({
+      invalid_type_error: "El valor debe ser un numero",
+    }).min(0, 'Debe ser mayor a 0'),
 })
   .refine((data) => data.uniformeHasta > data.uniformeDesde, {
     message: 'Uniforme Hasta debe ser mayor que Uniforme Desde',
